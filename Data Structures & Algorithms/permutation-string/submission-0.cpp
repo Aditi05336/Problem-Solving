08fefdd470c6a72c0723e9a1f19d0545f2cf8f85
Aditi <aditirajput0201@gmail.com>
2026-07-02 13:55:33 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n,m;
+        n= s1.length();
+        m = s2.length();
+        int right=0;
+        int left=0;
+        vector<int>mapp1(26,0);
+        for(char c:s1){
+            mapp1[c-'a']++;
+        }
+         vector<int>freq(26,0);
+        while(right<m){
+            freq[s2[right]-'a']++;
+            if(right-left+1==n){
+                    if(mapp1==freq){
+                        return true;
+                    }
+
+
+                freq[s2[left]-'a']--;
+                left++;
+            }
+
+            right++;
+
+
+        }
+        return false;
+        
+    }
+};
