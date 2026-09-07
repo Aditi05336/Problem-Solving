1f3100d364a98d2ddc42829a9f7cd7fd8634b244
Aditi <aditirajput0201@gmail.com>
2026-09-07 23:23:48 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& a) {
+        int n = a.size();
+
+        vector<int>dp(n+1,0);
+        dp[1]=a[0];
+        for(int i=2;i<n+1;i++){
+            dp[i]= max(dp[i-1],a[i-1]+dp[i-2]);
+        }
+        return dp[n];
+        
+    }
+};
